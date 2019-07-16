c090a6eda42de510b532e67ee8a77862c2911cf4
samuel <samuel@docotel.com>
2019-07-16 13:48:37 +0700

    12_Nesting Components

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import Ninja from './Ninja';
 
 function App() {
   return (
     <div className="App">
         <h1>My first react app!</h1>
+        <Ninja />
     </div>
   );
 }

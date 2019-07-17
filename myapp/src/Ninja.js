a2cba1c7352fa70bc04eb2656e47acb2273086eb
samuel <samuel@docotel.com>
2019-07-17 11:47:34 +0700

    15_Stateless Components

--- myapp/src/Ninja.js
+++ myapp/src/Ninja.js
@@ -1,26 +1,25 @@
-import React, { Component } from 'react';
+import React from 'react';
 
 
-class Ninja extends Component {
-    render() {
-        // console.log(this);
-        let {ninjas} = this.props;
-        
-        let ninja_list = ninjas.map((ninja) => {
-            return (
-                <div className="ninja" key={ninja.id}>
-                    <div>Name: {ninja.name}</div>
-                    <div>Age: {ninja.age}</div>
-                    <div>Belt: {ninja.belt}</div>
-                </div>
-            )
-        })
+const Ninja = (props) => {
+    // console.log(this);
+    let {ninjas} = props;
+    
+    let ninja_list = ninjas.map((ninja) => {
         return (
-            <div className='ninja-list'>
-                {ninja_list}
+            <div className="ninja" key={ninja.id}>
+                <div>Name: {ninja.name}</div>
+                <div>Age: {ninja.age}</div>
+                <div>Belt: {ninja.belt}</div>
             </div>
         )
-    }
+    })
+    return (
+        <div className='ninja-list'>
+            {ninja_list}
+        </div>
+    )
+
 }
 
 export default Ninja;

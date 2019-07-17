64b8d881e007b7671e795fd2050035327fabe694
samuel <samuel@docotel.com>
2019-07-17 10:53:21 +0700

    fix app

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,14 +1,17 @@
-import React from 'react';
+import React, {Component} from 'react';
 import Ninja from './Ninja';
 
-function App() {
-  return (
-    <div className="App">
-        <h1>My first react app!</h1>
-        <Ninja name="ryu" age="25" belt="black"/>
-        <Ninja name="yoshi" age="30" belt="test"/>
-    </div>
-  );
+class App extends Component{
+
+    render() {
+        return (
+          <div className="App">
+              <h1>My first react app!</h1>
+              <Ninja name="ryu" age="25" belt="black"/>
+              <Ninja name="yoshi" age="30" belt="test"/>
+          </div>
+        );
+    }
 }
 
 export default App;

6e112b783970d63e2521420af0078295acbc18c1
samuel <samuel@docotel.com>
2019-07-17 10:17:03 +0700

    13_Props

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -5,7 +5,8 @@ function App() {
   return (
     <div className="App">
         <h1>My first react app!</h1>
-        <Ninja />
+        <Ninja name="ryu" age="25" belt="black"/>
+        <Ninja name="yoshi" age="30" belt="test"/>
     </div>
   );
 }

af5350aa6768853bff74d3028fed235c757d0b64
samuel <samuel@docotel.com>
2019-07-17 14:33:22 +0700

    16 - Conditional Output

--- myapp/src/Ninja.js
+++ myapp/src/Ninja.js
@@ -5,20 +5,65 @@ const Ninja = (props) => {
     // console.log(this);
     let {ninjas} = props;
     
-    let ninja_list = ninjas.map((ninja) => {
-        return (
+
+    /**
+     * @way_1
+     */
+    // let ninja_list = ninjas.map((ninja) => {
+    //     if (ninja.age > 25) {
+    //         return (
+    //             <div className="ninja" key={ninja.id}>
+    //                 <div>Name: {ninja.name}</div>
+    //                 <div>Age: {ninja.age}</div>
+    //                 <div>Belt: {ninja.belt}</div>
+    //             </div>
+    //         );
+    //     } else {
+    //         return null;
+    //     }
+    // })
+
+
+     /**
+      * @way_2
+      */
+    const ninja_list = ninjas.map((ninja) => {
+        return (ninja.age > 25) ? 
+        (
             <div className="ninja" key={ninja.id}>
                 <div>Name: {ninja.name}</div>
                 <div>Age: {ninja.age}</div>
                 <div>Belt: {ninja.belt}</div>
             </div>
-        )
-    })
+        ) : null;
+    });
     return (
         <div className='ninja-list'>
             {ninja_list}
         </div>
     )
+
+
+
+    /**
+     * @way_3
+     */
+    // return (
+    //     <div className='ninja-list'>
+    //         {
+    //             ninjas.map((ninja) => {
+    //                 return (ninja.age > 25) ? 
+    //                 (
+    //                     <div className="ninja" key={ninja.id}>
+    //                         <div>Name: {ninja.name}</div>
+    //                         <div>Age: {ninja.age}</div>
+    //                         <div>Belt: {ninja.belt}</div>
+    //                     </div>
+    //                 ) : null;
+    //             })
+    //         }
+    //     </div>
+    // )
 
 }
 

223a90578240db785ec3ad5bd5f0f792bef51fc2
bhumong <samuel.bhumong@gmail.com>
2020-03-22 19:49:42 +0700

    HOC

--- poketimes/src/hoc/Rainbow.js
+++ poketimes/src/hoc/Rainbow.js
@@ -1,1 +1,17 @@
-import React from 'react'
+import React from 'react';
+
+const Rainbow = (WrappedComponent) => {
+    let colours = ['red', 'pink', 'orange', 'blue', 'green', 'yellew'];
+    let randomColor = colours[Math.floor(Math.random() * 5)];
+    let className = randomColor + '-text';
+
+    return (props) => {
+        return (
+            <div className={className}>
+                <WrappedComponent {...props}/>
+            </div>
+        )
+    }
+}
+
+export default Rainbow;

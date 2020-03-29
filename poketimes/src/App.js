31c0c8df1103890933ee05af1d68de33fa23eca8
bhumong <samuel.bhumong@gmail.com>
2020-03-29 17:48:49 +0700

    switch statment

--- poketimes/src/App.js
+++ poketimes/src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import Navbar from './Components/Navbar';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Home from './Components/Home';
 import Contact from './Components/Contact';
 import About from './Components/About';
@@ -12,10 +12,12 @@ class App extends Component {
             <BrowserRouter>
                 <div className="App">
                     <Navbar />
-                    <Route exact path='/' component={Home}/>
-                    <Route path='/about' component={About}/>
-                    <Route path='/contact' component={Contact}/>
-                    <Route path='/:post_id' component={Post} />
+                    <Switch>
+                        <Route exact path='/' component={Home}/>
+                        <Route path='/about' component={About}/>
+                        <Route path='/contact' component={Contact}/>
+                        <Route path='/:post_id' component={Post} />
+                    </Switch>
                 </div>
             </BrowserRouter>
         );
